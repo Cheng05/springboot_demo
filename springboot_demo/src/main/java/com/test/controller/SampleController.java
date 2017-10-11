b83691ab607fa58e88bff4c4eee34f850c3d253c
898591754qq.com <898591754qq.com>
2017-10-11 11:24:22 +0800

    first commit

---
+++ springboot_demo/src/main/java/com/test/controller/SampleController.java
@@ -0,0 +1,16 @@
+package com.test.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by zhaochuncheng on 2017/10/11.
+ */
+@RestController
+public class SampleController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
